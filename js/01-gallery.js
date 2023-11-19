d73164ed3913a8854c6c5008cf195a8f3061c26d
MalgorzataM <malgorzata.markulis@gmail.com>
2023-11-19 18:10:21 +0100

    correct

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,25 +1,37 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 
-// Change code below this line
-const galleryBox = document.querySelector("ul.gallery");
-
-let lightboxInstance = null;
-galleryBox.addEventListener("click", (event) => {
-    event.preventDefault();
-    if (event.target.tagName === "IMG") {
-        const imgSource = event.target.dataset.source;
-
-        lightboxInstance = basicLightbox.create(
-            `<img src="${imgSource}" alt="Image description">`
-        );
-
-        lightboxInstance.show();
-
-        window.addEventListener("keydown", handleKey);
+document.addEventListener('DOMContentLoaded', () => {
+    // Change code below this line
+    const selector = "ul.gallery";
+    const galleryBox = document.querySelector(selector);
+    if (galleryBox === null) {
+        console.error('Gallery container not found, using selector: ', selector);
+        return;
     }
-});
-for (const item of galleryItems) {
-    const imgGallery = `<div class="gallery__item">
+    let lightboxInstance = null;
+    galleryBox.addEventListener("click", (event) => {
+        event.preventDefault();
+        const et = event.target;
+        if (et === null) {
+            console.error(' Event target not found. event:', et)
+            return;
+        }
+
+        // @ts-ignore
+        if (et.tagName === "IMG") {
+            const imgSource = event.target.dataset.source;
+
+            lightboxInstance = basicLightbox.create(
+                `<img src="${imgSource}" alt="Image description">`
+            );
+
+            lightboxInstance.show();
+
+            window.addEventListener("keydown", handleKey);
+        }
+    });
+    for (const item of galleryItems) {
+        const imgGallery = `<div class="gallery__item">
    <a class="gallery__link" href="${item.original}">
    <img
     class="gallery__image"
@@ -29,14 +41,15 @@ for (const item of galleryItems) {
    />
    </a>
   </div>`;
-    galleryBox.insertAdjacentHTML("beforeend", imgGallery);
-}
-
-function handleKey(event) {
-    if (event.key === "Escape") {
-        lightboxInstance.close();
-        window.removeEventListener("keydown", handleKey);
+        galleryBox.insertAdjacentHTML("beforeend", imgGallery);
     }
-}
+    //klik esc
+    function handleKey(event) {
+        if (event.key === "Escape") {
+            lightboxInstance.close();
+            window.removeEventListener("keydown", handleKey);
+        }
+    }
 
-console.log(galleryItems);
+    console.log(galleryItems);
+})
